7fcb869c1e70e9af87f27b0385744cb7fb810e2e
Shinichi Kudo <rahadit.a.p123@gmail.com>
2023-06-17 11:18:10 +0700

    melanjutkan program sederhana

--- script.js
+++ script.js
@@ -6,35 +6,40 @@ let inputUser = prompt('Silahkan pilih MANUSIA , GAJAH, SEMUT');
 // Komputer memilih secara acak
 let comp = Math.random();
 if (comp <= 0.33){
-    comp = 'Manusia';
-}else if(comp > 0.33){
-    comp = 'Gajah';
+    comp = 'manusia';
+}else if(comp > 0.33 &&  comp < 0.66){
+    comp = 'gajah';
 } else {
-    comp = 'Semut';
+    comp = 'semut';
     
 }
 
 // Beri tahu user pilihan Komputer
-
 alert(`Lawanmu memilih ${comp}`);
 
 // Bandingkan pilihan Komputer dengan inputan user
-if (inputUser === comp){
+
+// jika pilihan user dan komputer sama maka akan seri
+if (inputUser == comp){
         alert('Kamu Seri !');
-} else if (inputUser === 'Gajah') {
-    if(comp == 'Manusia'){
+} 
+
+// jika pilihan user dan comp berbeda
+else if (inputUser === 'gajah') {
+    if(comp === 'manusia'){
         alert('Kamu Menang');
-    } else {
+    } 
+    else {
         alert('Kamu Kalah');
     }
-} else if (inputUser == 'Manusia') { 
-    if(comp == 'Semut'){
+} else if (inputUser === 'manusia') { 
+    if(comp === 'semut'){
         alert('Kamu Menang');
     } else {
         alert('Kamu Kalah');
     }
 } else {
-    if(comp == 'Gajah'){
+    if(comp == 'gajah'){
         alert('Kamu Menang');
     } else {
         alert('Kamu Kalah');
