6e48c59ba0a0f1b68e21687c31dba146ec7a5801
Shinichi Kudo <rahadit.a.p123@gmail.com>
2023-06-19 09:13:57 +0700

    menambhkan link pada icon dan mengaktifkan javascript

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
+
 // Ucapan selamat datang 
-alert('Selamat Datang di Game Suwit Jawa');
+// alert('Selamat Datang di Game Suwit Jawa');
 
 // interaksi kepada user apakah rigram akan diulangi
-let ulang = true;
+// let ulang = true;
 
-while(ulang){
+// while(ulang){
 
     // Perintahkan user untuk menginputkan pilihan 
-    let inputUser = prompt('Silahkan pilih MANUSIA , GAJAH, SEMUT');
+    // let inputUser = prompt('Silahkan pilih MANUSIA , GAJAH, SEMUT');
     
-    // Komputer memilih secara acak
-    let comp = Math.random();
-    if (comp <= 0.33){
-        comp = 'manusia';
-    }else if(comp > 0.33 &&  comp < 0.66){
-        comp = 'gajah';
-    } else {
-        comp = 'semut';
-    }
+    // // Komputer memilih secara acak
+    // let comp = Math.random();
+    // if (comp <= 0.33){
+    //     comp = 'manusia';
+    // }else if(comp > 0.33 &&  comp < 0.66){
+    //     comp = 'gajah';
+    // } else {
+    //     comp = 'semut';
+    // }
     
     // Beri tahu kepada user pilihan Komputer
-    alert(`Lawanmu memilih ${comp}`);
+    // alert(`Lawanmu memilih ${comp}`);
     
     // buat variabel untuk wadah hasil 
-    let hasil;
+    // let hasil;
     
     // Bandingkan pilihan Komputer dengan pilihan user
     
     // jika pilihan user dan komputer sama maka akan seri
-    if (inputUser == comp){
-        hasil = 'Kamu Seri !';
-    } 
+    // if (inputUser == comp){
+        // hasil = 'Kamu Seri !';
+    // } 
     // jika pilihan user dan comp berbeda
-        else {
+        // else {
             // menggunakan operasi ternary
-            hasil = ((inputUser == 'manusia' && comp == 'semut') || (inputUser == 'gajah' && comp == 'manusia') || (inputUser == 'semut' && comp == 'gajah')) ? 'Kamu Menang' : 'Kamu Kalah';
-        }
+            // hasil = ((inputUser == 'manusia' && comp == 'semut') || (inputUser == 'gajah' && comp == 'manusia') || (inputUser == 'semut' && comp == 'gajah')) ? 'Kamu Menang' : 'Kamu Kalah';
+        // }
     
     // tampilkan hasil permainan kepada user
-    alert(hasil);
+    // alert(hasil);
 
     
-    ulang = confirm('Apakah kamu akan mengulangi permainan ini lagi ?'); // ulang akan mereturn nilai true atau false
-}
+    // ulang = confirm('Apakah kamu akan mengulangi permainan ini lagi ?'); // ulang akan mereturn nilai true atau false
+// }
+
+
+// menangkap element gambar yang akan dipilih
+const pilihanUser = document.querySelectorAll('.container .main .user img');
+
+// jika gambar di klik oleh user
