6ddbbf971af16ce932022d09a95c84a81207e7d2
Rahaditya Abimanyu Putra <rahadi.a.p123@gmail.com>
2023-07-10 19:42:03 +0700

    commit pertama

--- script.js
+++ script.js
@@ -1,92 +1,67 @@
+// menangkap element gambar yang akan dipilih
+const gambarDipilih = document.querySelectorAll('.container .main .user .kotak-pilih div img');
 
-// Ucapan selamat datang 
-// alert('Selamat Datang di Game Suwit Jawa');
 
-// interaksi kepada user apakah rigram akan diulangi
-// let ulang = true;
+// console.log(pilihanUser);
 
-// while(ulang){
 
-    // Perintahkan user untuk menginputkan pilihan 
-    // let inputUser = prompt('Silahkan pilih MANUSIA , GAJAH, SEMUT');
-    
-    // // Komputer memilih secara acak
-    // let comp = Math.random();
-    // if (comp <= 0.33){
-    //     comp = 'manusia';
-    // }else if(comp > 0.33 &&  comp < 0.66){
-    //     comp = 'gajah';
-    // } else {
-    //     comp = 'semut';
-    // }
-    
-    // Beri tahu kepada user pilihan Komputer
-    // alert(`Lawanmu memilih ${comp}`);
-    
-    // buat variabel untuk wadah hasil 
-    // let hasil;
-    
-    // Bandingkan pilihan Komputer dengan pilihan user
-    
-    // jika pilihan user dan komputer sama maka akan seri
-    // if (inputUser == comp){
-        // hasil = 'Kamu Seri !';
-    // } 
-    // jika pilihan user dan comp berbeda
-        // else {
-            // menggunakan operasi ternary
-            // hasil = ((inputUser == 'manusia' && comp == 'semut') || (inputUser == 'gajah' && comp == 'manusia') || (inputUser == 'semut' && comp == 'gajah')) ? 'Kamu Menang' : 'Kamu Kalah';
-        // }
-    
-    // tampilkan hasil permainan kepada user
-    // alert(hasil);
+// menangkap element button
+const button = document.querySelector('button');
 
-    
-    // ulang = confirm('Apakah kamu akan mengulangi permainan ini lagi ?'); // ulang akan mereturn nilai true atau false
+// jika button di klik
+button.addEventListener('click', function() {
+  // jika gambar di klik oleh user maka akan mengembalikan sebuat class
+  gambarDipilih.forEach(function(e){
+    e.addEventListener('click', function(){
+      console.log(e.className);
+    })
+  })
+
+})
+
+
+      
+// Komputer memilih secara acak
+let comp = Math.random();
+if (comp <= 0.33){
+    comp = 'manusia';
+}else if(comp > 0.33 &&  comp < 0.66){
+    comp = 'gajah';
+} else {
+    comp = 'semut';
+}
+  // console.log(comp);
+
+// function versus(hasil){
+  
+
+//     if (user == comp){
+//         return 'Kamu Seri';
+//     } 
+  // jika pilihan user dan comp berbeda
+      // else {
+    // menggunakan operasi ternary
+      // return ((user == 'manusia' && comp == 'pilihSemut') || (user == 'pilihGajah' && comp == 'manusia') || (user == 'pilihSemut' && comp == 'pilihGajah')) ? 'Kamu Menang' : 'Kamu Kalah'}
 // }
 
 
-// menangkap element gambar yang akan dipilih
-let pilihanUser = Array.from(document.querySelectorAll('.container .main .user .kotak-pilih img'));
-let user;
-
-// jika gambar di klik oleh user
-for(let i = 0; i < pilihanUser.length; i++){
-    pilihanUser[i].addEventListener('click', function(){
-        user = pilihanUser[i].getAttribute('class');
-
-        // menangkap element button
-        let button = document.querySelector('button');
-        button.addEventListener('click', function(){
-            // Komputer memilih secara acak
-            let comp = Math.random();
-            if (comp <= 0.33){
-                comp = 'manusia';
-            }else if(comp > 0.33 &&  comp < 0.66){
-                comp = 'pilihGajah';
-            } else {
-                comp = 'pilihSemut';
-            }
-            console.log(comp);
-            if (user == comp){
-        hasil = 'Kamu Seri !';
-    } 
-    // jika pilihan user dan comp berbeda
-        else {
-            // menggunakan operasi ternary
-            hasil = ((user == 'manusia' && comp == 'pilihSemut') || (user == 'pilihGajah' && comp == 'manusia') || (user == 'pilihSemut' && comp == 'pilihGajah')) ? 'Kamu Menang' : 'Kamu Kalah';
-        }
 
 
-        // tampilkan hasil
-        const ucapan = document.querySelector('.main .hasil');
-        ucapan.innerHTML = hasil;
+// menangkap element untuk menampilkan hasil
+// const ucapan = document.querySelector('.container .main .hasil');
+
+// tampilkan hasil
+// ucapan.innerHTML = versus();
 
 
 
 
 
-        })
-    })
-    }
 
+
+
+// menangkap element button
+// let button = document.querySelector('button');
+// button.addEventListener('click', function(){
+
+// }
